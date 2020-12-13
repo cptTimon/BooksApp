75549d42f29535b21710fee31df5d3e3e1f449a7
Wojciech Kaczmarzyński <wojciech.kaczmarzynski@gmail.com>
2020-12-13 18:57:41 +0100

    Task 10.2_subTask.3

---
+++ src/js/script.js
@@ -0,0 +1,62 @@
+{
+  'use strict';
+
+  const select = {
+    wrapper: {
+      booksList: '.books-list',
+    },
+    templateOf: {
+      booksList: '#template-book',
+    },
+    class: {
+      favouriteBook: 'favorite',
+    },
+    book: {
+      bookImage: ''
+    },
+  };
+
+  const templates = {
+    booksList: Handlebars.compile(document.querySelector(select.templateOf.booksList).innerHTML),
+  };
+
+  const bookListWrapper = document.querySelector(select.wrapper.booksList);
+  const allBooks = [];
+  const favoriteBooks = [];
+
+  const renderLibrary = function(){
+    for(let book of dataSource.books){
+      const generatedHTML = templates.booksList(book);
+
+      const element = utils.createDOMFromHTML(generatedHTML);
+
+      bookListWrapper.appendChild(element);
+      allBooks.push(element);
+    }
+  };
+  console.log(allBooks);
+
+  const initActions = function(){
+    for(let book of allBooks){
+      const bookCover = book.querySelector('.book__image');
+      console.log(bookCover);
+
+      bookCover.addEventListener('dblclick', function(event){
+        event.preventDefault();
+        const id = bookCover.getAttribute('data-id');
+        if(!bookCover.classList.contains(select.class.favouriteBook)){
+          favoriteBooks.push(id);
+          bookCover.classList.add(select.class.favouriteBook);
+          console.log('dodalem ksiazke!');
+        } else {
+          favoriteBooks.splice(favoriteBooks.indexOf(id), 1);
+          bookCover.classList.remove(select.class.favouriteBook);
+        }
+        console.log('Favorite Books', favoriteBooks);
+      })
+    }
+  }
+  console.log('allBooks', allBooks);;
+  renderLibrary();
+  initActions();
+}
