3ffdbf935275049f3dd2fc7011b45ebfa371d894
Wojciech Kaczmarzyński <wojciech.kaczmarzynski@gmail.com>
2020-12-15 11:27:31 +0100

    OOP revision

--- src/js/script.js
+++ src/js/script.js
@@ -79,6 +79,23 @@
       }
     }
 
+    determineRatingBgc(rating){
+      let bgc = '';
+      if(rating <6){
+        bgc = 'linear-gradient(to bottom,  #fefcea 0%, #f1da36 100%);';
+      }
+      if(rating>6 && rating<=8){
+        bgc = 'linear-gradient(to bottom, #b4df5b 0%,#b4df5b 100%);';
+      }
+      if(rating>8 && rating<=9){
+        bgc = 'linear-gradient(to bottom, #299a0b 0%, #299a0b 100%);';
+      }
+      if(rating>9){
+        bgc = 'linear-gradient(to bottom, #ff0084 0%,#ff0084 100%);';
+      }
+      return bgc;
+    };
+
     initActions(){
       const thisBooksList = this;
       thisBooksList.bookListWrapper.addEventListener('dblclick', function(event){
@@ -116,98 +133,4 @@
     }
   }
   const app = new BooksList(); // eslint-disable-line no-unused-vars
-
-  /*const bookListWrapper = document.querySelector(select.wrapper.bookList);
-  const filterWrapper = document.querySelector(select.wrapper.filters);
-  const favoriteBooks = [];
-  const filters = []; */
-
-  /*const determineRatingBgc = function(rating){
-    let bgc = '';
-    if(rating <6){
-      bgc = 'linear-gradient(to bottom,  #fefcea 0%, #f1da36 100%);';
-    }
-    if(rating>6 && rating<=8){
-      bgc = 'linear-gradient(to bottom, #b4df5b 0%,#b4df5b 100%);';
-    }
-    if(rating>8 && rating<=9){
-      bgc = 'linear-gradient(to bottom, #299a0b 0%, #299a0b 100%);';
-    }
-    if(rating>9){
-      bgc = 'linear-gradient(to bottom, #ff0084 0%,#ff0084 100%);';
-    }
-    return bgc;
-  }; */
-
-  /*const renderLibrary = function(){
-    for(let book of dataSource.books){
-      const generatedHTML = templates.booksList(book);
-      //const ratingBgc = determineRatingBgc(book.rating);
-      const element = utils.createDOMFromHTML(generatedHTML);
-      bookListWrapper.appendChild(element);
-
-      const rating = element.querySelector('.book__rating__fill');
-      console.log(ratingBgc);
-      console.log(rating.getAttribute("style"));
-      rating.getAttribute("style")["background"] = ratingBgc;
-      console.log(rating.style);
-
-    }
-  }; */
-
-
-  /*const filterBooks = function(){
-    for(let book of dataSource.books){
-      const bookToBeHidden = document.querySelector('.book__image[data-id="' + book.id + '"]');
-      let shouldBeHidden = false;
-      for(let filterName of filters){
-        if(!book.details[filterName]){
-          shouldBeHidden = true;
-          break;
-        }
-      }
-      if(shouldBeHidden){
-        bookToBeHidden.classList.add('hidden');
-      } else {
-        bookToBeHidden.classList.remove('hidden');
-      }
-    }
-  }; */
-  /*const initActions = function(){
-
-    bookListWrapper.addEventListener('dblclick', function(event){
-      event.preventDefault();
-      const clickedElement = event.target.offsetParent;
-      console.log(clickedElement);
-      console.log(clickedElement.classList.contains('book__image'));
-
-      if(clickedElement.classList.contains('book__image')){
-        const id = clickedElement.getAttribute('data-id');
-        if(!clickedElement.classList.contains(classNames.favoriteBook)){
-          favoriteBooks.push(id);
-          clickedElement.classList.add(classNames.favoriteBook);
-        } else {
-          favoriteBooks.splice(favoriteBooks.indexOf(id), 1);
-          clickedElement.classList.remove(classNames.favoriteBook);
-        }
-      }
-      console.log(favoriteBooks);
-    });
-
-    filterWrapper.addEventListener('click', function(event){
-      const clickedElement = event.target;
-      console.log('filters',filters);
-      if(clickedElement.tagName === 'INPUT' && clickedElement.type === 'checkbox' && clickedElement.name === 'filter' ){
-        if(clickedElement.checked){
-          filters.push(clickedElement.value);
-          filterBooks();
-        } else {
-          filters.splice(filters.indexOf(clickedElement.value), 1);
-          filterBooks();
-        }
-      }
-      console.log('filters ponownie', filters);
-    });
-
-  };*/
 }
